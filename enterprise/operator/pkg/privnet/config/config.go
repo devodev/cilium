737c91d44f0840bc6c8b44241c03753837593b76
Tobias Klauser <tobias@isovalent.com>
2026-04-10 11:11:58 +0200

    privnet: operator: add auto-external-endpoints config
    Add the auto-external-endpoints configuration to the main operator
    config, akin to the already existing NAD integration config. This will
    be used in subsequent commits adding the actual auto-external-endpoints
    functionality.
    
    Signed-off-by: Tobias Klauser <tobias@isovalent.com>

--- enterprise/operator/pkg/privnet/config/config.go
+++ enterprise/operator/pkg/privnet/config/config.go
@@ -37,24 +37,42 @@ var (
 			CNILogFile:   filepath.Join(defaults.RuntimePath, "cilium-cni.log"),
 			CNILogFormat: string(logging.DefaultLogFormatTimestamp),
 		},
+
+		AutoExternalEndpoints: AutoExternalEndpointsConfig{
+			Enabled:          false,
+			ConfigDir:        filepath.Join(defaults.LibraryPath, "privnet", "auto-external-endpoints-config"),
+			SecretsNamespace: "cilium-secrets",
+		},
 	}
 )
 
 type Config struct {
 	config.Common `mapstructure:",squash"`
 
-	NADIntegration NADIntegrationConfig `mapstructure:",squash"`
+	NADIntegration        NADIntegrationConfig        `mapstructure:",squash"`
+	AutoExternalEndpoints AutoExternalEndpointsConfig `mapstructure:",squash"`
+}
+
+func (cfg Config) validate() error {
+	return cfg.NADIntegration.validate()
+}
+
+func (def Config) Flags(flags *pflag.FlagSet) {
+	def.Common.Flags(flags)
+	def.NADIntegration.Flags(flags)
+	def.AutoExternalEndpoints.Flags(flags)
+}
+
+// EnabledWithNADIntegration returns whether private networking is enabled, with
+// support for Multus network attachment definitions.
+func (cfg Config) EnabledWithNADIntegration() bool {
+	return cfg.Enabled && cfg.NADIntegration.Enabled
 }
 
 type NADIntegrationConfig struct {
 	Enabled      bool   `mapstructure:"private-networks-nad-integration-enabled"`
 	CNILogFile   string `mapstructure:"private-networks-nad-cni-log-file"`
 	CNILogFormat string `mapstructure:"private-networks-nad-cni-log-format"`
-}
-
-func (def Config) Flags(flags *pflag.FlagSet) {
-	def.Common.Flags(flags)
-	def.NADIntegration.Flags(flags)
 }
 
 func (def NADIntegrationConfig) Flags(flags *pflag.FlagSet) {
@@ -64,16 +82,6 @@ func (def NADIntegrationConfig) Flags(flags *pflag.FlagSet) {
 		"CNI logs path configured for managed Multus network attachment definitions")
 	flags.String("private-networks-nad-cni-log-format", def.CNILogFormat,
 		"CNI logs format configured for managed Multus network attachment definitions")
-}
-
-// EnabledWithNADIntegration returns whether private networking is enabled, with
-// support for Multus network attachment definitions.
-func (cfg Config) EnabledWithNADIntegration() bool {
-	return cfg.Enabled && cfg.NADIntegration.Enabled
-}
-
-func (cfg Config) validate() error {
-	return cfg.NADIntegration.validate()
 }
 
 func (cfg NADIntegrationConfig) validate() error {
@@ -87,4 +95,25 @@ func (cfg NADIntegrationConfig) validate() error {
 	}
 
 	return nil
+}
+
+// EnabledWithAutoExternalEndpoints returns whether private networking is enabled, with
+// support for automatic creation of PrivateNetworkExternalEndpoints.
+func (cfg Config) EnabledWithAutoExternalEndpoints() bool {
+	return cfg.Enabled && cfg.AutoExternalEndpoints.Enabled
+}
+
+type AutoExternalEndpointsConfig struct {
+	Enabled          bool   `mapstructure:"private-networks-auto-external-endpoints-enabled"`
+	ConfigDir        string `mapstructure:"private-networks-auto-external-endpoints-config-dir"`
+	SecretsNamespace string `mapstructure:"private-networks-auto-external-endpoints-secrets-namespace"`
+}
+
+func (cfg AutoExternalEndpointsConfig) Flags(flags *pflag.FlagSet) {
+	flags.Bool("private-networks-auto-external-endpoints-enabled", cfg.Enabled,
+		"Automatically create PrivateNetworkExternalEndpoints")
+	flags.String("private-networks-auto-external-endpoints-config-dir", cfg.ConfigDir,
+		"Configuration for the creation of PrivateNetworkExternalEndpoints")
+	flags.String("private-networks-auto-external-endpoints-secrets-namespace", cfg.SecretsNamespace,
+		"Kubernetes namespace from which provider secrets are read")
 }
