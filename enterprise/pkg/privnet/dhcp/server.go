devodev/cilium

commit f242e27b60d9d7975f3f417e299e63beb5f0e55b

privnet: dhcp: be more resilient when waiting for cilium_dhcp
Issue #10668 reported a failed recvfrom with "network is down".
Since we are waiting for the device to exist and the logs showed
that we resolved the ifindex then the only other explanation I could
come up for this was that we didn't wait for the interface to be
set UP.

Change [Server.setup] to also check that the interface is UP before
proceeding and increase the wait time to 1 minute and respect the
context so we don't block shutdown.

Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

enterprise/pkg/privnet/dhcp/server.go
@@ -119,7 +119,7 @@ func (r *requestRateLimiter) allow(id uint16) bool {
 }
 
 func (s *Server) Serve(ctx context.Context, health cell.Health) error {
-	if err := s.setup(); err != nil {
+	if err := s.setup(ctx); err != nil {
 		return err
 	}
 	defer s.conn.Close()
@@ -233,7 +233,10 @@ func (s *Server) Serve(ctx context.Context, health cell.Health) error {
 	}
 }
 
-func (s *Server) setup() error {
+func (s *Server) setup(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Minute)
+	defer cancel()
+
 	target := s.netns
 	if target == nil {
 		var err error
@@ -247,12 +250,17 @@ func (s *Server) setup() error {
 	return target.Do(func() error {
 		var err error
 		var link netlink.Link
-		for range 10 {
+		// Wait until the device exists and is UP
+		for ctx.Err() == nil {
 			link, err = safenetlink.LinkByName(s.ifname)
-			if err == nil {
+			if err == nil && link.Attrs().OperState == netlink.OperUp {
 				break
 			}
-			time.Sleep(100 * time.Millisecond)
+			select {
+			case <-ctx.Done():
+				err = ctx.Err()
+			case <-time.After(100 * time.Millisecond):
+			}
 		}
 		if err != nil {
 			return fmt.Errorf("lookup interface %q: %w", s.ifname, err)

commit 5e1f8a432a6d6e74f6532c99713342ebe7dabd42

privnet: dhcp: check flags not state on cilium_dhcp
The cilium_dhcp is a dummy device which doesn't report operational state.
Check the flags for FlagUp instead.

Fixes: f242e27b60d9 ("privnet: dhcp: be more resilient when waiting for cilium_dhcp")
Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

enterprise/pkg/privnet/dhcp/server.go
@@ -253,7 +253,7 @@ func (s *Server) setup(ctx context.Context) error {
 		// Wait until the device exists and is UP
 		for ctx.Err() == nil {
 			link, err = safenetlink.LinkByName(s.ifname)
-			if err == nil && link.Attrs().OperState == netlink.OperUp {
+			if err == nil && link.Attrs().Flags&net.FlagUp != 0 {
 				break
 			}
 			select {