ee505e13a0fe34f5a891759a3e0da8f00351bf7a
Deepesh Pathak <deepeshpathak09@gmail.com>
2026-04-13 14:43:06 -0700

    enterprise: implement additional policy diagnostic conditions
    This commit adds additional policy related diagnostic conditions:
    
    * policy_missing_proxy_redirects: Condition to monitor if any endpoint
      policies are failed to configure proxy redirects.
    * policy_parsing_failures: Condition to monitor any policy related
      parsing failures.
    * xds_failures: Condition to monitor if any xDS response to envoy proxy
      are rejected.
    
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- enterprise/pkg/policy/diagnostics/policy.go
+++ enterprise/pkg/policy/diagnostics/policy.go
@@ -38,19 +38,36 @@ func registerPolicyDiagnosticConditions(reg *diagnostics.Registry) error {
 			Description: "Enforcing policy updates from control plane is taking longer than expected.",
 			Evaluator:   evalPolicyImplmentationDelay,
 		},
+
 		diagnostics.Condition{
 			ID:          "policy_identity_updates",
 			SubSystem:   "Policy",
 			Description: "Propagation of identity updates to endpoint policy state is taking longer than expected.",
 			Evaluator:   evalPolicyIncrementalUpdateLatency,
 		},
+
+		diagnostics.Condition{
+			ID:          "policy_parsing_failures",
+			SubSystem:   "Policy",
+			Description: "Failures observed in parsing control plane policy objects(NP, KCNP, CNP, CCNP)",
+			Evaluator:   evalPolicyParsingFailures,
+		},
+
+		diagnostics.Condition{
+			ID:          "xds_failures",
+			SubSystem:   "Envoy",
+			Description: "Failures observed in propagating xDS config updates to Envoy",
+			Evaluator:   evalEnvoyXDSFailures,
+		},
+
+		diagnostics.Condition{
+			ID:          "policy_missing_proxy_redirects",
+			SubSystem:   "Policy",
+			Description: "Endpoint policies have missing proxy redirects",
+			Evaluator:   evalPolicyMissingProxyRedirects,
+		},
 	)
 }
-
-// TODO: Add support for sampling counter metrics as rate/increase stats in diagnostics module.
-// Support diagnostic condition for counter metrics:
-// * `cilium_policy_change_total{outcome="failure"}` - Policy parsing failures
-// * `cilium_xds_events_count{status="nack"}` - Envoy config update failures
 
 func evalPolicyImplmentationDelay(env diagnostics.Environment) (msg string, severity diagnostics.Severity) {
 	metricName := metrics.PolicyImplementationDelay.Opts().ConfigName
@@ -101,5 +118,78 @@ func evalPolicyIncrementalUpdateLatency(env diagnostics.Environment) (msg string
 		return fmt.Sprintf("Current average policy identity update propagation latency %.1fs is >%.1fx the 24 hour average of %.1fs",
 			stats.Avg_Latest, multiplier, stats.Avg_24h), diagnostics.Minor
 	}
-	return fmt.Sprintf("%.2fs OK", stats.Avg_Latest), diagnostics.OK
+	return fmt.Sprintf("Policy incremental update latency OK (average %.2fs)", stats.Avg_Latest), diagnostics.OK
+}
+
+func evalPolicyParsingFailures(env diagnostics.Environment) (msg string, severity diagnostics.Severity) {
+	metricName := metrics.PolicyChangeTotal.Opts().ConfigName
+	failureMetrics, err := env.MetricsMatchingLabels(metricName, map[string]string{
+		metrics.LabelOutcome: metrics.LabelValueOutcomeFailure,
+	})
+	if err != nil {
+		return err.Error(), diagnostics.OK
+	}
+
+	var (
+		totalFailures     int64
+		failingConditions []string
+	)
+	for _, m := range failureMetrics {
+		stats, err := env.Counter(metricName, m.Labels())
+		if err != nil {
+			continue
+		}
+		if stats.Count_Latest > 0 {
+			totalFailures += stats.Count_Latest
+			failingConditions = append(failingConditions, fmt.Sprintf("%s: %d", m.LabelsString(), stats.Count_Latest))
+		}
+	}
+
+	if totalFailures > 0 {
+		return fmt.Sprintf("Policy parsing failures observed: %d [%s]", totalFailures, strings.Join(failingConditions, ", ")), diagnostics.Minor
+	}
+	return "No policy parsing failures", diagnostics.OK
+}
+
+func evalEnvoyXDSFailures(env diagnostics.Environment) (msg string, severity diagnostics.Severity) {
+	metricName := metrics.Namespace + "_xds_events_count"
+	nackMetrics, err := env.MetricsMatchingLabels(metricName, map[string]string{
+		"status": "nack",
+	})
+	if err != nil {
+		// xDS Events count metric is not registered by default.
+		return fmt.Sprintf(`No %s{status="nack"} metric`, metricName), diagnostics.OK
+	}
+
+	var (
+		totalNACKs        int64
+		failingConditions []string
+	)
+	for _, m := range nackMetrics {
+		stats, err := env.Counter(metricName, m.Labels())
+		if err != nil {
+			continue
+		}
+		if stats.Count_Latest > 0 {
+			totalNACKs += stats.Count_Latest
+			failingConditions = append(failingConditions, fmt.Sprintf("%s: %d", m.LabelsString(), stats.Count_Latest))
+		}
+	}
+
+	if totalNACKs > 0 {
+		return fmt.Sprintf("Rejected envoy xDS response observed: %d [%s]", totalNACKs, strings.Join(failingConditions, ", ")), diagnostics.Minor
+	}
+	return "No xDS failures", diagnostics.OK
+}
+
+func evalPolicyMissingProxyRedirects(env diagnostics.Environment) (msg string, severity diagnostics.Severity) {
+	metricName := metrics.PolicyMissingProxyRedirects.Opts().ConfigName
+	stats, err := env.Gauge(metricName, nil)
+	if err != nil {
+		return err.Error(), diagnostics.OK
+	}
+	if stats.Avg_Latest > 0 {
+		return fmt.Sprintf("%.0f proxy redirects are missing for endpoint policies", stats.Avg_Latest), diagnostics.Minor
+	}
+	return "No missing proxy redirects", diagnostics.OK
 }
