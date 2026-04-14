cc55835582fdbc1a1efb2a2bb725cad85344933d
Marco Iorio <marco.iorio@isovalent.com>
2026-04-14 15:25:29 +0200

    privnet/tests: extend inventory to support Multi-NIC VMs
    Extend the mock inventory to support Multi-NIC VMs. Most notably,
    we need to adapt the rendering logic to support a variable number
    of interfaces. No functional changes are performed as part of this
    commit, and the specification of additional interfaces is deferred.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- contrib/testing/privnet/mock-inventory.py
+++ contrib/testing/privnet/mock-inventory.py
@@ -10,139 +10,156 @@
 
 import os
 import socket
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 port = os.getenv("CILIUM_PRIVNET_PORT", default="8000")
 dns_server = os.getenv("CILIUM_PRIVNET_DNS_SERVER", default="8.8.8.8")
 
-network_tmpl = """
-{{
-  "id": "{id}",
-  "path": "/v-bridge/network/{name}",
-  "name": "{name}",
-  "selfLink": "/providers/mock/networks/{id}",
-  "variant": "Standard"
-}}
-"""
 
-vm_tmpl = """
-{{
-  "id": "{id}",
-  "path": "/v-bridge/vm/{id}",
-  "name": "{name}",
-  "selfLink": "/providers/mock/vms/{id}",
-  "revisionValidated": 535,
-  "networks": [
-    {{
-      "kind": "Network",
-      "id": "{netID}"
-    }}
-  ],
-  "cpuCount": 2,
-  "memoryMB": 1024,
-  "guestName": "Ubuntu Linux (64-bit)",
-  "guestId": "ubuntu64Guest",
-  "ipAddress": "{ip4}",
-  "devices": [
-    {{
-      "kind": "VirtualVmxnet3"
-    }}
-  ],
-  "nics": [
-    {{
-      "network": {{
-        "kind": "Network",
-        "id": "{netID}"
-      }},
-      "mac": "{mac}"
-    }}
-  ],
-  "guestNetworks": [
-    {{
-      "device": "0",
-      "mac": "{mac}",
-      "ip": "{ip4}",
-      "prefix": 24
-    }},
-    {{
-      "device": "0",
-      "mac": "{mac}",
-      "ip": "{ip6}",
-      "prefix": 128
-    }},
-    {{
-      "device": "0",
-      "mac": "{mac}",
-      "ip": "fe80::250:56ff:fe8c:af6f",
-      "prefix": 64
-    }}
-  ],
-  "guestIpStacks": [
-    {{
-      "device": "0",
-      "dns": ["{dns}"]
-    }}
-  ],
-  "secureBoot": false
-}}
-"""
+def render_network(id, info):
+    return {
+        "id": id,
+        "path": f"/v-bridge/network/{info['name']}",
+        "name": info["name"],
+        "selfLink": f"/providers/mock/networks/{id}",
+        "variant": "Standard",
+    }
+
+
+def render_vm(id, info):
+    ifaces = info["interfaces"]
+    dns = info["dns"]
+
+    guest_networks = []
+    for idx, iface in enumerate(ifaces):
+        guest_networks.extend([
+            {
+                "device": str(idx),
+                "mac": iface["mac"],
+                "ip": iface["ip4"],
+                "prefix": 24,
+            },
+            {
+                "device": str(idx),
+                "mac": iface["mac"],
+                "ip": iface["ip6"],
+                "prefix": 128,
+            },
+            {
+                "device": str(idx),
+                "mac": iface["mac"],
+                "ip": "fe80::250:56ff:fe8c:af6f",
+                "prefix": 64,
+            },
+        ])
+
+    return {
+        "id": id,
+        "path": f"/v-bridge/vm/{id}",
+        "name": info["name"],
+        "selfLink": f"/providers/mock/vms/{id}",
+        "revisionValidated": 535,
+        "networks": [
+            {"kind": "Network", "id": iface["netID"]} for iface in ifaces
+        ],
+        "cpuCount": 2,
+        "memoryMB": 1024,
+        "guestName": "Ubuntu Linux (64-bit)",
+        "guestId": "ubuntu64Guest",
+        "ipAddress": ifaces[0]["ip4"],
+        "devices": [{"kind": "VirtualVmxnet3"} for iface in ifaces],
+        "nics": [
+            {
+                "network": {"kind": "Network", "id": iface["netID"]},
+                "mac": iface["mac"],
+            }
+            for iface in ifaces
+        ],
+        "guestNetworks": guest_networks,
+        "guestIpStacks": [
+            {"device": str(idx), "dns": [dns]} for idx, _ in enumerate(ifaces)
+        ],
+        "secureBoot": False,
+    }
+
 
 data = {
     "network-01": {
-        "tmpl": network_tmpl,
+        "render": render_network,
         "name": "network-a",
     },
     "network-02": {
-        "tmpl": network_tmpl,
+        "render": render_network,
         "name": "network-b",
     },
     "network-03": {
-        "tmpl": network_tmpl,
+        "render": render_network,
         "name": "network-c",
     },
     "vm-A0": {
-        "tmpl": vm_tmpl,
+        "render": render_vm,
         "name": "test-webhook",
-        "netID": "network-01",
-        "ip4": "192.168.250.16",
-        "ip6": "fd10:0:250::80",
-        "mac": "00:50:56:8c:af:6f",
+        "interfaces": [
+            {
+                "netID": "network-01",
+                "ip4": "192.168.250.16",
+                "ip6": "fd10:0:250::80",
+                "mac": "00:50:56:8c:af:6f",
+            }
+        ],
         "dns": dns_server,
     },
     "vm-A1": {
-        "tmpl": vm_tmpl,
+        "render": render_vm,
         "name": "client-network-a",
-        "netID": "network-01",
-        "ip4": "192.168.250.10",
-        "ip6": "fd10:0:250::10",
-        "mac": "f2:54:1c:1f:84:94",
+        "interfaces": [
+            {
+                "netID": "network-01",
+                "ip4": "192.168.250.10",
+                "ip6": "fd10:0:250::10",
+                "mac": "f2:54:1c:1f:84:94",
+            }
+        ],
         "dns": dns_server,
     },
     "vm-A2": {
-        "tmpl": vm_tmpl,
+        "render": render_vm,
         "name": "echo-same-node-network-a",
-        "netID": "network-01",
-        "ip4": "192.168.250.20",
-        "ip6": "fd10:0:250::20",
-        "mac": "de:a9:fd:7d:af:bf",
+        "interfaces": [
+            {
+                "netID": "network-01",
+                "ip4": "192.168.250.20",
+                "ip6": "fd10:0:250::20",
+                "mac": "de:a9:fd:7d:af:bf",
+            }
+        ],
         "dns": dns_server,
     },
     "vm-A3": {
-        "tmpl": vm_tmpl,
+        "render": render_vm,
         "name": "echo-other-node-network-a",
-        "netID": "network-01",
-        "ip4": "192.168.250.21",
-        "ip6": "fd10:0:250::21",
-        "mac": "be:68:f6:fc:6a:4a",
+        "interfaces": [
+            {
+                "netID": "network-01",
+                "ip4": "192.168.250.21",
+                "ip6": "fd10:0:250::21",
+                "mac": "be:68:f6:fc:6a:4a",
+            }
+        ],
         "dns": dns_server,
     },
     "vm-B1": {
-        "tmpl": vm_tmpl,
+        "render": render_vm,
         "name": "client-network-b",
-        "netID": "network-02",
-        "ip4": "192.168.251.10",
-        "ip6": "fd10:0:251::10",
-        "mac": "42:f9:eb:33:4d:54",
+        "interfaces": [
+            {
+                "netID": "network-02",
+                "ip4": "192.168.251.10",
+                "ip6": "fd10:0:251::10",
+                "mac": "42:f9:eb:33:4d:54",
+            }
+        ],
         "dns": dns_server,
     },
 }
@@ -173,11 +190,11 @@ class Handler(BaseHTTPRequestHandler):
 
         try:
             info = data[id]
-            info["id"] = id
 
             self.send_response(200)
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(info["tmpl"].format(**info).encode())
+            self.wfile.write(json.dumps(info["render"](id, info)).encode())
 
         except KeyError as e:
             self.send_response(404, "Not found")
