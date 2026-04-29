e0c824c17dc10a4e557b37d6b4e93d5d234d9776
Martynas Pumputis <martynas@isovalent.com>
2026-04-29 11:23:07 +0200

    tunnelip: Check whether WG or IPSec are enabled
    See code comments for making both mutually exclusive.
    
    Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

--- enterprise/pkg/tunnelip/cell.go
+++ enterprise/pkg/tunnelip/cell.go
@@ -12,22 +12,47 @@ package tunnelip
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
 	"github.com/spf13/pflag"
 
 	dpipc "github.com/cilium/cilium/pkg/datapath/ipcache"
+	ipsectypes "github.com/cilium/cilium/pkg/datapath/linux/ipsec/types"
 	"github.com/cilium/cilium/pkg/node"
 	nodemanager "github.com/cilium/cilium/pkg/node/manager"
+	wgtypes "github.com/cilium/cilium/pkg/wireguard/types"
 )
+
+const preferredTunnelEndpointDevicesFlag = "preferred-tunnel-endpoint-devices"
 
 type Config struct {
 	PreferredTunnelEndpointDevices []string
 }
 
 func (def Config) Flags(flags *pflag.FlagSet) {
-	flags.StringSlice("preferred-tunnel-endpoint-devices", def.PreferredTunnelEndpointDevices, "Devices to prefer for tunneling. Supports '+' as wildcard in device name, e.g. 'eth+'")
+	flags.StringSlice(preferredTunnelEndpointDevicesFlag, def.PreferredTunnelEndpointDevices, "Devices to prefer for tunneling. Supports '+' as wildcard in device name, e.g. 'eth+'")
+}
+
+func (cfg Config) Validate(wgConfig wgtypes.Config, ipsecConfig ipsectypes.Config) error {
+	if len(cfg.PreferredTunnelEndpointDevices) == 0 {
+		return nil
+	}
+
+	// In tunneling mode WireGuard encrypts encapsulated packets. Since WG is unaware of NodeCiliumTunnelIP
+	// (it is not part of AllowedPeers), such packets will be dropped.
+	if wgConfig.Enabled() {
+		return fmt.Errorf("WireGuard encryption cannot be used with --%s", preferredTunnelEndpointDevicesFlag)
+	}
+
+	// In tunneling mode IPSec configures XFRM policies with NodeIPs. Encapsulated packed with NodeCiliumTunnelIP
+	// will bypass the policies, and thus won't be encrypted.
+	if ipsecConfig.Enabled() {
+		return fmt.Errorf("IPSec encryption cannot be used with --%s", preferredTunnelEndpointDevicesFlag)
+	}
+
+	return nil
 }
 
 var defaultConfig = Config{}
@@ -37,6 +62,8 @@ var Cell = cell.Module(
 	"Custom tunnel endpoints",
 
 	cell.Config(defaultConfig),
+
+	cell.Invoke(Config.Validate),
 
 	cell.Invoke(func(p params, listener *dpipc.BPFListener, nodes nodemanager.NodeManager, jobs job.Group, localNodeStore *node.LocalNodeStore) {
 		mgr := newManager(p)
