58a14e2ffa366e7af5dff4299639c9fbea0f0480
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-05-26 09:28:29 +0900

    bgp: Override OSS Hive commands only when both BGP CPlane are enabled
    Override the OSS Hive commands (bgp/routes and bgp/route-policies) only
    when both OSS and enterprise BGP Control Plane are enabled. This keeps
    the OSS BGP Control Plane behavior intact when only OSS BGP CPlane is
    enabled.
    
    When only enterprise BGP CPlane is enabled, provide enterprise command
    directly without relying on the OSS mechanism.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- enterprise/pkg/bgpv1/commands/cell.go
+++ enterprise/pkg/bgpv1/commands/cell.go
@@ -11,22 +11,65 @@
 package commands
 
 import (
+	"github.com/cilium/hive"
 	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/script"
 
+	"github.com/cilium/cilium/enterprise/operator/pkg/bgpv2/config"
 	"github.com/cilium/cilium/enterprise/pkg/bgpv1/agent"
 	"github.com/cilium/cilium/enterprise/pkg/bgpv1/manager/reconcilerv2"
 	ossCommands "github.com/cilium/cilium/pkg/bgp/commands"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 // Override the OSS BGP commands with the enterprise-extended versions
-var Cell = cell.DecorateAll(
-	func(
-		bgpMgr agent.EnterpriseBGPRouterManager,
-		errorPathStore *reconcilerv2.ErrorPathStore,
-		ossCmds ossCommands.BGPCommands,
-	) ossCommands.BGPCommands {
-		ossCmds["bgp/routes"] = BGPRoutesExtendedCmd(bgpMgr, errorPathStore)
-		ossCmds["bgp/route-policies"] = BGPPRoutePolicies(bgpMgr)
-		return ossCmds
-	},
+var Cell = cell.Group(
+	cell.DecorateAll(
+		func(
+			dc *option.DaemonConfig,
+			config config.Config,
+			bgpMgr agent.EnterpriseBGPRouterManager,
+			errorPathStore *reconcilerv2.ErrorPathStore,
+			ossCmds ossCommands.BGPCommands,
+		) ossCommands.BGPCommands {
+			if dc.BGPControlPlaneEnabled() && config.Enabled {
+				// Override the OSS BGP commands with the
+				// enterprise-extended versions when both OSS and
+				// enterprise BGP Control Plane are enabled.
+				ossCmds["bgp/routes"] = BGPRoutesCmd(bgpMgr, errorPathStore)
+				ossCmds["bgp/route-policies"] = BGPPRoutePolicies(bgpMgr)
+			}
+			return ossCmds
+		},
+	),
+	cell.Provide(
+		func(
+			dc *option.DaemonConfig,
+			config config.Config,
+			bgpMgr agent.EnterpriseBGPRouterManager,
+			errorPathStore *reconcilerv2.ErrorPathStore,
+		) hive.ScriptCmdsOut {
+			if !config.Enabled {
+				// If enterprise BGP Control Plane is disabled,
+				// do not provide any commands.
+				return hive.ScriptCmdsOut{}
+			}
+
+			if dc.BGPControlPlaneEnabled() {
+				// If both OSS and enterprise BGP Control Plane
+				// are enabled, the enterprise-extended BGP
+				// commands will be provided by the decorator
+				// above, so no need to provide them here.
+				return hive.ScriptCmdsOut{}
+			}
+
+			// If only enterprise BGP Control Plane is enabled,
+			// provide the enterprise-extended BGP commands
+			// directly.
+			return hive.NewScriptCmds(map[string]script.Cmd{
+				"bgp/routes":         BGPRoutesCmd(bgpMgr, errorPathStore),
+				"bgp/route-policies": BGPPRoutePolicies(bgpMgr),
+			})
+		},
+	),
 )
