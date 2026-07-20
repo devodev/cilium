devodev/cilium

commit 6851a6759c43d66029a3c7640b979c9f41995eff

privnet: fix pruning of stale ctmap pins
The current CT maps reconciler pruning logic is not actually effective;
indeed, the logic purges stale entries from the `ctMaps` field, which
is guaranteed to be always already aligned by construction, as well as
from the maps of maps, which are always recreated upon startup, and
hence guaranteed to be initially empty. Yet, it does not implement the
removal of possible stale ctmap pins, left behind in case the Cilium
configuration changed (e.g., because a previously enabled IP family is
disabled, or a private network is deleted while the Cilium agent is
down). Hence, let's rework the logic to collect the set of expected
pins, based on the known private networks, and remove any leftover
pin that matches the expected format, but is stale. The script test
is also extended to validate the correct pruning.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/ctmaps.go
@@ -16,7 +16,11 @@ import (
 	"errors"
 	"fmt"
 	"iter"
+	"log/slog"
 	"maps"
+	"os"
+	"path/filepath"
+	"regexp"
 	"slices"
 	"strings"
 	"text/tabwriter"
@@ -34,6 +38,7 @@ import (
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
 	ctmapgc "github.com/cilium/cilium/pkg/maps/ctmap/gc"
 	"github.com/cilium/cilium/pkg/maps/timestamp"
@@ -41,6 +46,9 @@ import (
 )
 
 type CTMapFactory func(name string, cfg ctmap.MapConfig, opts ...ctmap.MapOption) pnmaps.CTMap
+
+// CTMapTCPathOverride allows to override the default map pins path for testing purposes.
+type CTMapTCPathOverride string
 
 var CTMapsCell = cell.Group(
 	cell.ProvidePrivate(
@@ -83,7 +91,10 @@ var CTMapsCell = cell.Group(
 type CTMaps struct {
 	mu lock.Mutex
 
+	log *slog.Logger
+
 	factory CTMapFactory
+	tcPath  string
 	ctMaps  map[tables.NetworkID]*ctMap
 	global  ctmap.CTMaps
 
@@ -103,10 +114,12 @@ type CTMaps struct {
 func newCTMaps(in struct {
 	cell.In
 
+	Log       *slog.Logger
 	Lifecycle cell.Lifecycle
 
 	Global  ctmap.CTMaps
 	Factory CTMapFactory
+	TCPath  CTMapTCPathOverride `optional:"true"`
 
 	TCP4 pnmaps.CTMapsMapTCP4
 	Any4 pnmaps.CTMapsMapAny4
@@ -114,8 +127,10 @@ func newCTMaps(in struct {
 	Any6 pnmaps.CTMapsMapAny6
 }) *CTMaps {
 	var maps = &CTMaps{
-		global:  in.Global,
+		log: in.Log,
+
 		factory: in.Factory,
+		tcPath:  cmp.Or(string(in.TCPath), bpf.TCGlobalsPath()),
 		ctMaps:  make(map[tables.NetworkID]*ctMap),
 
 		tcp4:    in.TCP4,
@@ -400,17 +415,6 @@ func (c *CTMaps) deleteCTMapLocked(networkID tables.NetworkID) error {
 	)
 }
 
-func (c *CTMaps) pruneCTMapsLocked(alive sets.Set[tables.NetworkID]) error {
-	var err error
-	for networkID := range c.ctMaps {
-		if !alive.Has(networkID) {
-			err = errors.Join(err, c.deleteCTMapLocked(networkID))
-		}
-	}
-
-	return err
-}
-
 func ctKeyVal(networkID tables.NetworkID, m pnmaps.CTMap) *pnmaps.CTMapsKeyVal {
 	return &pnmaps.CTMapsKeyVal{
 		Key: pnmaps.CTMapsKey{NetworkID: uint32(networkID)},
@@ -549,77 +553,43 @@ func (c *CTMaps) Delete(ctx context.Context, txn statedb.ReadTxn, revision state
 	return err
 }
 
-type ctKeyValWithRev struct {
-	*pnmaps.CTMapsKeyVal
-	Revision statedb.Revision
-}
+// Prune removes all CT maps no longer alive
+func (c *CTMaps) Prune(context.Context, statedb.ReadTxn, iter.Seq2[tables.ConnTrackMap, statedb.Revision]) error {
+	// Collect the name of all expected maps.
+	c.mu.Lock()
 
-func iterCtKeyVals(objs []ctKeyValWithRev) iter.Seq2[*pnmaps.CTMapsKeyVal, statedb.Revision] {
-	return func(yield func(*pnmaps.CTMapsKeyVal, statedb.Revision) bool) {
-		for _, obj := range objs {
-			if !yield(obj.CTMapsKeyVal, obj.Revision) {
-				return
+	var expected = sets.New[string]()
+	for _, m := range c.ctMaps {
+		for _, ctm := range m.all() {
+			expected.Insert(ctm.Name())
+		}
+	}
+
+	c.mu.Unlock()
+
+	pins, err := os.ReadDir(c.tcPath)
+	if err != nil {
+		return fmt.Errorf("listing pinned BPF maps: %w", err)
+	}
+
+	var matcher = regexp.MustCompile(
+		fmt.Sprintf("^%s_[0-9]{5}(%s|%s|%s|%s)$", mapPrefix,
+			mapSuffixTCP4, mapSuffixTCP6, mapSuffixAny4, mapSuffixAny6,
+		),
+	)
+
+	// Iterate over all pinned maps, and delete any that match the name format,
+	// but are not expected to exist.
+	for _, entry := range pins {
+		if !entry.IsDir() && !expected.Has(entry.Name()) && matcher.MatchString(entry.Name()) {
+			err2 := os.Remove(filepath.Join(c.tcPath, entry.Name()))
+			if err2 != nil && !os.IsNotExist(err2) {
+				err = errors.Join(err, fmt.Errorf("unpinning %s map: %w", entry.Name(), err2))
+			} else if err2 == nil {
+				c.log.Info("Pruned stale CT map", logfields.Name, entry.Name())
 			}
 		}
 	}
-}
-
-// Prune removes all CT maps no longer alive
-func (c *CTMaps) Prune(ctx context.Context, txn statedb.ReadTxn, objs iter.Seq2[tables.ConnTrackMap, statedb.Revision]) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	var err error
-	retainedNetworks := make(sets.Set[tables.NetworkID])
-	var tcp4Objs, any4Objs, tcp6Objs, any6Objs []ctKeyValWithRev
-	for obj, rev := range objs {
-		m, ok := c.ctMaps[obj.NetworkID]
-		if !ok {
-			err = errors.Join(err, fmt.Errorf("prune: no private network CT map for network %q", obj.Network))
-			continue
-		}
-
-		if c.tcp4.Enabled() {
-			tcp4Objs = append(tcp4Objs, ctKeyValWithRev{ctKeyVal(obj.NetworkID, m.tcp4), rev})
-		}
-		if c.any4.Enabled() {
-			any4Objs = append(any4Objs, ctKeyValWithRev{ctKeyVal(obj.NetworkID, m.any4), rev})
-		}
-		if c.tcp6.Enabled() {
-			tcp6Objs = append(tcp6Objs, ctKeyValWithRev{ctKeyVal(obj.NetworkID, m.tcp6), rev})
-		}
-		if c.any6.Enabled() {
-			any6Objs = append(any6Objs, ctKeyValWithRev{ctKeyVal(obj.NetworkID, m.any6), rev})
-		}
-
-		retainedNetworks.Insert(obj.NetworkID)
-	}
-
-	pruneEntries := func(
-		name string,
-		outerMap pnmaps.Map[*pnmaps.CTMapsKeyVal],
-		entries []ctKeyValWithRev,
-		ops reconciler.Operations[*pnmaps.CTMapsKeyVal],
-	) error {
-		if !outerMap.Enabled() {
-			return nil
-		}
-
-		err := ops.Prune(ctx, txn, iterCtKeyVals(entries))
-		if err != nil {
-			return fmt.Errorf("%s: %w", name, err)
-		}
-
-		return nil
-	}
-
-	err = errors.Join(err,
-		pruneEntries("tcp4", c.tcp4, tcp4Objs, c.tcp4Ops),
-		pruneEntries("any4", c.any4, any4Objs, c.any4Ops),
-		pruneEntries("tcp6", c.tcp6, tcp6Objs, c.tcp6Ops),
-		pruneEntries("any6", c.any6, any6Objs, c.any6Ops),
-		c.pruneCTMapsLocked(retainedNetworks),
-	)
 
 	return err
 }

enterprise/pkg/privnet/tests/mock_ctmaps.go
@@ -17,6 +17,7 @@ import (
 	"maps"
 	"net/netip"
 	"os"
+	"path/filepath"
 	"reflect"
 	"slices"
 	"strconv"
@@ -46,10 +47,11 @@ func mockCTMaps(t testing.TB) cell.Cell {
 	t.Helper()
 
 	return cell.Group(
-		cell.ProvidePrivate(newCTMapsRegistry),
+		cell.ProvidePrivate(func() *ctMapsRegistry { return newCTMapsRegistry(t) }),
 		cell.DecorateAll((*ctMapsRegistry).factory),
 		cell.DecorateAll((*ctMapsRegistry).toCTMaps),
 		cell.Provide(func() ctmap.CTMaps { return nil }),
+		cell.Provide((*ctMapsRegistry).pinPathOverride),
 		cell.Provide((*ctMapsRegistry).commands),
 	)
 }
@@ -60,12 +62,15 @@ type ctMapsRegistry struct {
 	registry map[string]*ctMap
 	failures lock.Map[string, struct{}]
 	nextFD   int
+
+	pinPath string
 }
 
-func newCTMapsRegistry() *ctMapsRegistry {
+func newCTMapsRegistry(t testing.TB) *ctMapsRegistry {
 	var registry = ctMapsRegistry{
 		registry: make(map[string]*ctMap),
 		nextFD:   1,
+		pinPath:  t.TempDir(),
 	}
 
 	registry.new(ctmap.MapNameTCP4Global, ctmap.MapConfig{TCP: true, IPv6: false})
@@ -107,6 +112,10 @@ func (r *ctMapsRegistry) new(name string, cfg ctmap.MapConfig, _ ...ctmap.MapOpt
 func (r *ctMapsRegistry) factory() reconcilers.CTMapFactory { return r.new }
 func (r *ctMapsRegistry) toCTMaps() pnmaps.CTMaps           { return r }
 
+func (r *ctMapsRegistry) pinPathOverride() reconcilers.CTMapTCPathOverride {
+	return reconcilers.CTMapTCPathOverride(r.pinPath)
+}
+
 func (r *ctMapsRegistry) ActiveMapsGlobal() []pnmaps.CTMap {
 	return cslices.Map(r.activeMapsMatching("cilium_ct"),
 		func(m pnmaps.CTMapWithConfig) pnmaps.CTMap { return m.Map })
@@ -133,6 +142,8 @@ func (r *ctMapsRegistry) commands() hive.ScriptCmdsOut {
 	return hive.NewScriptCmds(
 		map[string]script.Cmd{
 			"privnet/ct-maps-registry/list":             r.dump(),
+			"privnet/ct-maps-registry/create-pin":       r.createPin(),
+			"privnet/ct-maps-registry/list-pins":        r.listPins(),
 			"privnet/ct-maps-registry/inject-failure":   r.injectFailure(),
 			"privnet/ct-maps-registry/withdraw-failure": r.withdrawFailure(),
 			"privnet/ct-maps-registry/map/show":         r.showMap(),
@@ -214,6 +225,60 @@ func (r *ctMapsRegistry) withdrawFailure() script.Cmd {
 			}
 
 			return nil, nil
+		},
+	)
+}
+
+func (r *ctMapsRegistry) createPin() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "Create 'fake' map pins to validate pruning",
+			Args:    "map...",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			for _, name := range args {
+				err := os.WriteFile(filepath.Join(r.pinPath, name), nil, 0644)
+				if err != nil {
+					return nil, fmt.Errorf("creating file: %w", err)
+				}
+			}
+
+			return nil, nil
+		},
+	)
+}
+
+func (r *ctMapsRegistry) listPins() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "List the 'fake' map pins to validate pruning",
+			Flags: func(fs *pflag.FlagSet) {
+				fs.StringP("out", "o", "", "File to write to instead of stdout")
+			},
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			outfile, err := s.Flags.GetString("out")
+			if err != nil {
+				return nil, fmt.Errorf("reading out flag: %w", err)
+			}
+
+			return func(*script.State) (stdout, stderr string, err error) {
+				entries, err := os.ReadDir(r.pinPath)
+				if err != nil {
+					return "", "", fmt.Errorf("listing fake pins: %w", err)
+				}
+
+				var b strings.Builder
+				for _, entry := range entries {
+					fmt.Fprintln(&b, entry.Name())
+				}
+
+				if outfile != "" {
+					return "", "", os.WriteFile(s.Path(outfile), []byte(b.String()), 0644)
+				}
+
+				return b.String(), "", nil
+			}, nil
 		},
 	)
 }

enterprise/pkg/privnet/tests/testdata/ctmaps.txtar
@@ -1,10 +1,29 @@
 #! --private-networks-enabled
 
+# Create a few map pins to validate pruning.
+privnet/ct-maps-registry/create-pin cilium_privnet_00001_ct4_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00001_ct6_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00001_ct_any4_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00001_ct_any6_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00009_ct4_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00009_ct6_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00009_ct_any4_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00009_ct_any6_global
+
+# Create a few more that don't match the format, and should not be removed.
+privnet/ct-maps-registry/create-pin cilium_privnet_02_ct4_global
+privnet/ct-maps-registry/create-pin cilium_privnet_00002_ct6_global_foo
+privnet/ct-maps-registry/create-pin foo_cilium_privnet_00002_ct_any4_global
+privnet/ct-maps-registry/create-pin cilium_privnet_ct_any6_global
+
+# Add an already existing private network, before starting the hive.
+k8s/add icpn-1.yaml
+
 hive start
 db/initialized private-networks privnet-ctmaps
 
-# Add two private networks...
-k8s/add icpn-1.yaml icpn-2.yaml
+# Add one more private networks...
+k8s/add icpn-2.yaml
 
 # ... and check that they are reflected into the ctmaps table,
 db/cmp privnet-ctmaps ctmaps.v1.table
@@ -22,6 +41,10 @@ privnet/maps-dump privnet_ct_any4_global
 * cmp stdout ct_any4_global.v1.txt
 privnet/maps-dump privnet_ct_any6_global
 * cmp stdout ct_any6_global.v1.txt
+
+# Assert that stale map pins have been pruned.
+privnet/ct-maps-registry/list-pins
+* cmp stdout ct-maps-pins.txt
 
 # Add one more private network...
 k8s/add icpn-3.yaml
@@ -310,4 +333,16 @@ cilium_privnet_00004_ct_any6_global  (0x14) - IPv4: false, TCP: false - pinned
 0x1 -> fd=8
 0x3 -> fd=16
 0x4 -> fd=20
+-- EOR --
+
+
+-- ct-maps-pins.txt --
+cilium_privnet_00001_ct4_global
+cilium_privnet_00001_ct6_global
+cilium_privnet_00001_ct_any4_global
+cilium_privnet_00001_ct_any6_global
+cilium_privnet_00002_ct6_global_foo
+cilium_privnet_02_ct4_global
+cilium_privnet_ct_any6_global
+foo_cilium_privnet_00002_ct_any4_global
 -- EOR --

commit b0cc0ff090b16534272b1bd4baa161374204665b

privnet: fix missing assignment of CTMaps field
The blamed commit incorrectly removed the assignment of the `global`
field of the CTMaps struct, but we didn't notice in tests because the
function using it is overridden in script tests, and migrations are
not exercised yet by E2E tests. Let's assign it again.

Fixes: 6851a6759c43 ("privnet: fix pruning of stale ctmap pins")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/ctmaps.go
@@ -132,6 +132,7 @@ func newCTMaps(in struct {
 		factory: in.Factory,
 		tcPath:  cmp.Or(string(in.TCPath), bpf.TCGlobalsPath()),
 		ctMaps:  make(map[tables.NetworkID]*ctMap),
+		global:  in.Global,
 
 		tcp4:    in.TCP4,
 		tcp4Ops: in.TCP4.Ops(),